ad099c4b505e6e8f0c68297c7ca8caed8eaec814
hahabuntun <hahabuntun@gmail.com>
2024-09-29 16:19:19 +0300

    changed device modiefier to pirvate

---
+++ app/src/main/java/com/example/conditionalslab3/8.kt
@@ -0,0 +1,22 @@
+open class SmartDevice4(val name: String, val category: String) {
+    private var deviceStatus = "online"
+
+    open val deviceType = "unknown"
+
+    constructor(name: String, category: String, statusCode: Int) : this(name, category) {
+        deviceStatus = when (statusCode) {
+            0 -> "offline"
+            1 -> "online"
+            else -> "unknown"
+        }
+    }
+
+
+    open fun turnOn() {
+        println("Smart device is turned on.")
+    }
+
+    open fun turnOff() {
+        println("Smart device is turned off.")
+    }
+}
