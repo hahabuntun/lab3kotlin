b801657e0869a56a4ae9ff855ddcd72595ca53ff
hahabuntun <hahabuntun@gmail.com>
2024-09-29 14:40:11 +0300

    same example but with when block

--- app/src/main/java/com/example/conditionalslab3/3.kt
+++ app/src/main/java/com/example/conditionalslab3/3.kt
@@ -8,4 +8,14 @@ fun main(){
         else "Invalid traffic-light color"
 
     println(message)
+
+    val trafficLightColor2 = "Amber"
+
+    val message2 = when(trafficLightColor2) {
+        "Red" -> "Stop"
+        "Yellow", "Amber" -> "Slow"
+        "Green" -> "Go"
+        else -> "Invalid traffic-light color"
+    }
+    println(message2)
 }
