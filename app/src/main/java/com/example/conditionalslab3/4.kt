3b20394a2b5041ed6fcea69230f692321dfa5190
hahabuntun <hahabuntun@gmail.com>
2024-09-29 14:53:24 +0300

    example of if else expression

--- app/src/main/java/com/example/conditionalslab3/4.kt
+++ app/src/main/java/com/example/conditionalslab3/4.kt
@@ -25,4 +25,15 @@ fun main() {
     } else {
         println("You didn't input a name.")
     }
+
+
+    var favoriteActor6: String? = "Sandra Oh"
+
+    val lengthOfName = if (favoriteActor6 != null) {
+        favoriteActor6.length
+    } else {
+        0
+    }
+
+    println("The number of characters in your favorite actor's name is $lengthOfName.")
 }
