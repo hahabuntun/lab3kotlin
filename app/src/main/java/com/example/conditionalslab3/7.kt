d165e2aed97661e4d43afafdaf2f78900842e9b6
hahabuntun <hahabuntun@gmail.com>
2024-09-29 16:05:19 +0300

    added subclasses to smart device

---
+++ app/src/main/java/com/example/conditionalslab3/7.kt
@@ -0,0 +1,66 @@
+open class SmartDevice3(val name: String, val category: String) {
+    var deviceStatus = "online"
+    constructor(name: String, category: String, statusCode: Int) : this(name, category) {
+        deviceStatus = when (statusCode) {
+            0 -> "offline"
+            1 -> "online"
+            else -> "unknown"
+        }
+    }
+
+
+    fun turnOn() {
+        println("Smart device is turned on.")
+    }
+
+    fun turnOff() {
+        println("Smart device is turned off.")
+    }
+}
+
+
+
+class SmartTvDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice3(name = deviceName, category = deviceCategory) {
+    var speakerVolume = 2
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+    var channelNumber = 1
+        set(value) {
+            if (value in 0..200) {
+                field = value
+            }
+        }
+    fun increaseSpeakerVolume() {
+        speakerVolume++
+        println("Speaker volume increased to $speakerVolume.")
+    }
+    fun nextChannel() {
+        channelNumber++
+        println("Channel number increased to $channelNumber.")
+    }
+}
+
+class SmartLightDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice3(name = deviceName, category = deviceCategory) {
+    var brightnessLevel = 0
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+    fun increaseBrightness() {
+        brightnessLevel++
+        println("Brightness increased to $brightnessLevel.")
+    }
+}
+
+
+fun main() {
+    SmartDevice3("Android TV", "Entertainment")
+    SmartDevice3(name = "Android TV", category = "Entertainment")
+
+}
