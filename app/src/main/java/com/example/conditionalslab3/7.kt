12b0f3abe7fc64364068805f47ec63ff0816ff5e
hahabuntun <hahabuntun@gmail.com>
2024-09-29 16:16:45 +0300

    overriding properties

--- app/src/main/java/com/example/conditionalslab3/7.kt
+++ app/src/main/java/com/example/conditionalslab3/7.kt
@@ -1,5 +1,7 @@
 open class SmartDevice3(val name: String, val category: String) {
     var deviceStatus = "online"
+    open val deviceType = "unknown"
+
     constructor(name: String, category: String, statusCode: Int) : this(name, category) {
         deviceStatus = when (statusCode) {
             0 -> "offline"
@@ -22,6 +24,8 @@ open class SmartDevice3(val name: String, val category: String) {
 
 class SmartTvDevice(deviceName: String, deviceCategory: String) :
     SmartDevice3(name = deviceName, category = deviceCategory) {
+    override val deviceType = "Smart TV"
+
     var speakerVolume = 2
         set(value) {
             if (value in 0..100) {
@@ -59,6 +63,7 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) :
 
 class SmartLightDevice(deviceName: String, deviceCategory: String) :
     SmartDevice3(name = deviceName, category = deviceCategory) {
+    override val deviceType = "Smart Light"
     var brightnessLevel = 0
         set(value) {
             if (value in 0..100) {
