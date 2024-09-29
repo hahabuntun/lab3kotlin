ac0360901371482c19a17441fecf4f6912c89ae4
hahabuntun <hahabuntun@gmail.com>
2024-09-29 14:35:12 +0300

    added is condition to check type to when block

--- app/src/main/java/com/example/conditionalslab3/2.kt
+++ app/src/main/java/com/example/conditionalslab3/2.kt
@@ -20,16 +20,25 @@ fun main(){
     val y = 3
 
     when (y) {
-        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
-        else -> println("x isn't a prime number between 1 and 10.")
+        2, 3, 5, 7 -> println("y is a prime number between 1 and 10.")
+        else -> println("y isn't a prime number between 1 and 10.")
     }
 
 
     val z = 3
 
     when (z) {
-        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
-        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
-        else -> println("x isn't a prime number between 1 and 10.")
+        2, 3, 5, 7 -> println("z is a prime number between 1 and 10.")
+        in 1..10 -> println("z is a number between 1 and 10, but not a prime number.")
+        else -> println("z isn't a prime number between 1 and 10.")
+    }
+
+    val h: Any = 20
+
+    when (h) {
+        2, 3, 5, 7 -> println("h is a prime number between 1 and 10.")
+        in 1..10 -> println("h is a number between 1 and 10, but not a prime number.")
+        is Int -> println("h is an integer number, but not between 1 and 10.")
+        else -> println("h isn't an integer number.")
     }
 }
