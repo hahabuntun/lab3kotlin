ccc038cdb706320de6c18a89a3896be927cea792
hahabuntun <hahabuntun@gmail.com>
2024-09-29 14:26:17 +0300

    fixed the bug

--- app/src/main/java/com/example/conditionalslab3/1.kt
+++ app/src/main/java/com/example/conditionalslab3/1.kt
@@ -5,13 +5,15 @@ fun main() {
     println(1 > 1)
     println(1 >= 1)
     println(1 != 1)
-    val trafficLightColor = "Green"
+    val trafficLightColor = "Black"
 
     if (trafficLightColor == "Red") {
         println("Stop")
     } else if (trafficLightColor == "Yellow") {
         println("Slow")
-    } else {
+    } else if (trafficLightColor == "Green") {
         println("Go")
+    } else {
+        println("Invalid traffic-light color")
     }
 }
