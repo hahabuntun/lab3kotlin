f40f878a56dec65ab547c200366182e0396f09be
hahabuntun <hahabuntun@gmail.com>
2024-09-29 17:56:00 +0300

    created trick or treat function

--- app/src/main/java/com/example/conditionalslab3/10.kt
+++ app/src/main/java/com/example/conditionalslab3/10.kt
@@ -1,13 +1,28 @@
 fun main() {
 //    val trickFunction = ::trick
-    val trickFunction = trick
+//    val trickFunction = trick
+//    trickFunction()
+//    treat()
+
+
+    val treatFunction = trickOrTreat(false)
+    val trickFunction = trickOrTreat(true)
+    treatFunction()
     trickFunction()
-    treat()
 }
 
 //fun trick() {
 //    println("No treats!")
 //}
+
+fun trickOrTreat(isTrick: Boolean): () -> Unit {
+    if (isTrick){
+        return trick
+    }
+    else{
+        return treat
+    }
+}
 
 val trick = {
     println("No treats!")
